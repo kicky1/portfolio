74656460d15b379dd4d44600e80992375fc39b17
Krzysztof Wicki <s171703@student.pg.edu.pl>
2023-08-03 13:59:53 +0200

    Uninstall contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,4 +8,4 @@ const nextConfig = {
 	},
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
